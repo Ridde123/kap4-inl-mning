9d060635e3f988f5affd33d73338a312768f9b9e
Pingvu <nhlpool27@gmail.com>
2023-11-23 13:14:27 +0100

    Add project files.

---
+++ kap4 inlämning/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Uppgift_4._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i < 30; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+            for (int i = 200; i >= 180; i--)
+            {
+                Console.WriteLine(i);
+
+            }
+            Console.WriteLine();
+            for (int i = 1000; i >= 1400; i += 50)
+            {
+                Console.WriteLine((int)i);
+            }
+        }
+    }
+}
+
+
+
+
